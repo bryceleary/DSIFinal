b173da4ee021082f841b143eb775c3890dc12239
YOUR FULL NAME <milika.robbins@gmail.com>
2020-01-06 16:28:23 -0500

    Edits to titles

--- Data/Cleaning Syntax_FDI.R
+++ Data/Cleaning Syntax_FDI.R
@@ -3,26 +3,26 @@ library(tidyverse)
 library(dplyr)
 library(here)
 library(magrittr)
-here("DSIfinal", "data", "Cleaning.OECD.FAO.DevAidDisburse")
-DevAidDisburse <- Cleaning.OECD.FAO.DevAidDisburse
+here("data", "Cleaning.OECD.FAO.DevAidDisburse")
+FDI <- Cleaning.OECD.FAO.DevAidDisburse
 
 #Delete Variables
-DevAidDisburse <- select(DevAidDisburse, -Domain, -Element, -Element.Code, -Item, -Year.Code, -Unit, -Flag, -Flag.Description, -Note)
+FDI <- select(DevAidDisburse, -Domain, -Element, -Element.Code, -Item, -Year.Code, -Unit, -Flag, -Flag.Description, -Note)
 
 #Rename Variables - incomplete
-DevAidDisburse <- rename(DevAidDisburse, flow = Value)
-DevAidDisburse <- rename(DevAidDisburse, recipient = Recipient.Country)
-DevAidDisburse <- rename(DevAidDisburse, donor = Donor)
-DevAidDisburse <- rename(DevAidDisburse, donor_code = Donor.Code)
+FDI <- rename(DevAidDisburse, flow = Value)
+FDI <- rename(DevAidDisburse, recipient = Recipient.Country)
+FDI <- rename(DevAidDisburse, donor = Donor)
+FDI <- rename(DevAidDisburse, donor_code = Donor.Code)
 
 #Initial Analysis
-diff(range(DevAidDisburse$flow, na.rm = FALSE))
-mean(DevAidDisburse$flow, na.rm = FALSE)
-median(DevAidDisburse$flow, na.rm = FALSE)
-sd(DevAidDisburse$flow, na.rm = FALSE)
-quantile(DevAidDisburse$flow, na.rm = FALSE, 0.25)
-quantile(DevAidDisburse$flow, na.rm = FALSE, 0.75)
-IQR(DevAidDisburse$flow, na.rm = FALSE)
+diff(range(FDI$flow, na.rm = FALSE))
+mean(FDI$flow, na.rm = FALSE)
+median(FDI$flow, na.rm = FALSE)
+sd(FDI$flow, na.rm = FALSE)
+quantile(FDI$flow, na.rm = FALSE, 0.25)
+quantile(FDI$flow, na.rm = FALSE, 0.75)
+IQR(FDI$flow, na.rm = FALSE)
 
 
 
