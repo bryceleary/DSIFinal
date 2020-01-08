756736a7f087def7ef75222120db7f5b0e9b87c8
YOUR FULL NAME <milika.robbins@gmail.com>
2020-01-08 18:11:24 -0500

    Updating for merge syntax

--- Code/Merging syntax.R
+++ Code/Merging syntax.R
@@ -1,14 +1,6 @@
-#Read cleaned data files
-FDIfinal <- read.csv(here("data", "Final_fdi_data.csv"))
-DevAid <- read.csv(here("data", "Final_devaid_data.csv"))
-Nutrition <- read.csv(here("data", "Final_nutrition_data.csv"))
-Trade <- read.csv(here("data", "Final_trade_data."))
-Depth <- read.csv(here("data", "Final_depth_data."))
-  
-  
 #Merge code
 FDIfinal %>% 
-  oin_all(list(FDIFinal,DevAid, Nutrition, Trade), type = "full")  %>% 
-  replace_na(list(FDINet=0, Value=0, )) %>%
+  full_join(list(FDIFinal,DevAid, Nutrition, Trade, Depth), type = "full")  %>% 
+  replace_na(list(FDINet="0", Value="0", Depth="0", Nut)) %>%
   arrange(Type)
 
