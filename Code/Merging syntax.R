786ac66aee59357f8c006ba07e0a6f1e297b9907
YOUR FULL NAME <milika.robbins@gmail.com>
2020-01-08 17:28:53 -0500

    Update to merge sytnax

--- Code/Merging syntax.R
+++ Code/Merging syntax.R
@@ -1,9 +1,9 @@
 #Read cleaned data files
-FDIfinal <- read.csv(here("data", "Final_faostat_fdi_data.csv"))
-DevAid <- read.csv(here("data", "Final_faostat_devaid_data.csv"))
-Nutrition <- read.csv(here("data", "Final_faostat_nutrition_data.csv"))
-Trade <- read.csv(here("data", "Final_faostat_trade_data."))
-Depth <- read.csv(here("data", "Final_faostat_depth_data."))
+FDIfinal <- read.csv(here("data", "Final_fdi_data.csv"))
+DevAid <- read.csv(here("data", "Final_devaid_data.csv"))
+Nutrition <- read.csv(here("data", "Final_nutrition_data.csv"))
+Trade <- read.csv(here("data", "Final_trade_data."))
+Depth <- read.csv(here("data", "Final_depth_data."))
   
   
 #Merge code
