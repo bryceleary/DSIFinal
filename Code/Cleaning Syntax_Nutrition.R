b1f0fc7a61a854f6bc37ee51995836627bbaae4e
bryceleary <leary.bryce@gmail.com>
2020-01-07 20:00:42 -0500

    Updated clean to create year variable

--- Code/Cleaning Syntax_Nutrition.R
+++ Code/Cleaning Syntax_Nutrition.R
@@ -3,17 +3,22 @@ library(tidyverse)
 library(dplyr)
 library(here)
 library(magrittr)
-nutrition <- read.csv(here("data", "Raw_Nutrition_Data.csv"))
+Nutrition <- read.csv(here("data", "Raw_Nutrition-Data.csv"))
 
 #Delete Unnecessary Variables
 Nutrition <- select(Nutrition, -Domain.Code, -Domain, -Element.Code, -Element, -Item.Code, -Item, -Unit, -Flag, -Flag.Description, -Note)
 
 #Rename variables
-nutrition <- rename(Nutrition, country_code = Area.Code)
-nutrition <- rename(Nutrition, country = Area)
-nutrition <- rename(Nutrition, adesa = Value)
-nutrition <- rename(Nutrition, year = Year)
-nutrition <- rename(Nutrition, year_code = Year.Code)
+Nutrition <- rename(Nutrition, country_code = Area.Code)
+Nutrition <- rename(Nutrition, country = Area)
+Nutrition <- rename(Nutrition, adesa = Value)
+Nutrition <- rename(Nutrition, year_spread = Year)
+Nutrition <- rename(Nutrition, year = Year.Code)
+
+#create year variable
+Nutrition <- Nutrition %>% separate(year, into = c("year1", "year3"), sep = 4, convert = TRUE)
+Nutrition <- mutate(Nutrition, year = ((year3 + year1)/2))
+Nutrition <- Nutrition %>% mutate(year1=NULL, year3=NULL)
 
 #Initial Analysis
 diff(range(Nutrition$adesa, na.rm = TRUE))
