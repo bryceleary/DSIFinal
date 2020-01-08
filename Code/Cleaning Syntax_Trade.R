b601e0e95c0ec5e31e48a44df2c0deec8d320226
acarine <aycarine@gmail.com>
2020-01-08 15:20:25 -0500

    Added Cleaned Trade Data

--- Code/Cleaning Syntax_Trade.R
+++ Code/Cleaning Syntax_Trade.R
@@ -3,17 +3,17 @@ library(tidyverse)
 library(dplyr)
 library(here)
 library(magrittr)
-Trade <- read.csv(here("data", "Cleaning_Trade_Data.csv"))
+Trade <- read.csv(here("data", "Raw_FAOSTAT_WestAfrica_Trade Data.csv"))
 
 #Delete Unnecessary Variables
-Trade <- select(Trade, -Domain.Code, -Domain, -Element.Code, -Item.Code, -Item, -Unit, -Flag, -Flag.Description)
-Trade <- select(Trade, -year_code)
+Trade <- select(Trade, -Domain.Code, -Domain, -Element.Code, -Item.Code, -Year.Code, -Unit, -Flag, -Flag.Description)
+
 
 #Rename variables
 Trade <- rename(Trade, country_code = Area.Code)
 Trade <- rename(Trade, country = Area)
 Trade <- rename(Trade, agtrade = Value)
-Trade <- rename(Trade, year_code = Year.Code)
+Trade <- rename(Trade, impexp = Element)
 
 #Initial analysis by year
 year_agtrade_summ <- group_by(Trade, Year) %>% summarize("mean" = mean(agtrade), 
