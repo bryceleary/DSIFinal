75be525bdfe4c96bde363d465ec9aeb50304675c
bryceleary <leary.bryce@gmail.com>
2020-01-07 20:23:26 -0500

    Added summary stats grouped by year for adesa

--- Code/Cleaning Syntax_Nutrition.R
+++ Code/Cleaning Syntax_Nutrition.R
@@ -20,7 +20,17 @@ Nutrition <- Nutrition %>% separate(year, into = c("year1", "year3"), sep = 4, c
 Nutrition <- mutate(Nutrition, year = ((year3 + year1)/2))
 Nutrition <- Nutrition %>% mutate(year1=NULL, year3=NULL)
 
-#Initial Analysis
+#Initial analysis by year
+year_adesa_summ <- group_by(Nutrition, year) %>% summarize("mean" = mean(adesa), 
+                                        "range" = diff(range(adesa)), 
+                                        "median" = median(adesa), 
+                                        "sd" = sd(adesa), 
+                                        "25th" = quantile(adesa, 0.25), 
+                                        "75th" = quantile(adesa, 0.75), 
+                                        "IQR" = IQR(adesa))
+
+
+#Initial Analysis (old, only as a whole)
 diff(range(Nutrition$adesa, na.rm = TRUE))
 mean(Nutrition$adesa, na.rm = TRUE)
 median(Nutrition$adesa, na.rm = TRUE)
