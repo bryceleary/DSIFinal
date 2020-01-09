e343ad68950fb41c6fa2d7e7ae7b4fdbeabd562d
Bryce Leary <leary.bryce@gmail.com>
2020-01-09 13:48:17 -0500

    Created R file for combined cleaning and prep code

---
+++ Code/cleaning-dataset-creation.R
@@ -0,0 +1,19 @@
+#Set-up
+library(tidyverse)
+library(dplyr)
+library(here)
+library(magrittr)
+
+#Data Import
+
+
+#Clean Depth dataset
+
+
+#Clean DevAid dataset
+
+
+#Clean FDI dataset
+
+
+#Clean Trade dataset
