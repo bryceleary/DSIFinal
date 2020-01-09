fdb0714af28a7507706346353c16cbac61a7f6ce
Bryce Leary <leary.bryce@gmail.com>
2020-01-09 12:08:53 -0500

    Corrected a warning in new merge syntax

--- Code/05_Merging syntax.R
+++ Code/05_Merging syntax.R
@@ -1,4 +1,4 @@
 #Merge code
 agriculture = merge(DevAid, FDIfinal, by = c("year", "country"))
-agriculture2 = merge(DevAid, FDIfinal, by = c("year", "country"), suffixes = c("",""))
-#Warning message: In merge.data.frame(DevAid, FDIfinal, by = c("Year", "country"), :column names 'type', 'country_code' are duplicated in the result
+agriculture2 = merge(DevAid, FDIfinal, by = c("year", "country", "country_code"), suffixes = c("",""))
+#Warning message: In merge.data.frame(DevAid, FDIfinal, by = c("Year", "country"), :column names 'type' are duplicated in the result
