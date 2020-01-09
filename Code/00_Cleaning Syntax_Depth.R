9d0b40d691faddc40d2763cf723c3685cad1faaa
Bryce Leary <leary.bryce@gmail.com>
2020-01-09 18:07:27 -0500

    Updated rename code

--- Code/00_Cleaning Syntax_Depth.R
+++ Code/00_Cleaning Syntax_Depth.R
@@ -9,27 +9,28 @@ Depth <- read.csv(here("data", "Cleaning_FAO_Depth_Data.csv"))
 Depth <- select(Depth, -Country.Code, -Indicator.Name, -Indicator.Code, -starts_with("X19"))
 
 #Rename variables
-Depth <- rename(Depth, country  = ï..Country.Name)
-Depth <- rename(Depth, "2000" = X2000)
-Depth <- rename(Depth, "2001" = X2001)
-Depth <- rename(Depth, "2002" = X2002)
-Depth <- rename(Depth, "2003" = X2003)
-Depth <- rename(Depth, "2004" = X2004)
-Depth <- rename(Depth, "2005" = X2005)
-Depth <- rename(Depth, "2006" = X2006)
-Depth <- rename(Depth, "2007" = X2007)
-Depth <- rename(Depth, "2008" = X2008)
-Depth <- rename(Depth, "2009" = X2009)
-Depth <- rename(Depth, "2010" = X2010)
-Depth <- rename(Depth, "2011" = X2011)
-Depth <- rename(Depth, "2012" = X2012)
-Depth <- rename(Depth, "2013" = X2013)
-Depth <- rename(Depth, "2014" = X2014)
-Depth <- rename(Depth, "2015" = X2015)
-Depth <- rename(Depth, "2016" = X2016)
-Depth <- rename(Depth, "2017" = X2017)
-Depth <- rename(Depth, "2018" = X2018)
-Depth <- rename(Depth, "2019" = X2019)
+Depth <- Depth %>%
+  rename(country  = ï..Country.Name,
+         "2000" = X2000,
+         "2001" = X2001,
+         "2002" = X2002,
+         "2003" = X2003,
+         "2004" = X2004,
+         "2005" = X2005,
+         "2006" = X2006,
+         "2007" = X2007,
+         "2008" = X2008,
+         "2009" = X2009,
+         "2010" = X2010,
+         "2011" = X2011,
+         "2012" = X2012,
+         "2013" = X2012,
+         "2014" = X2013,
+         "2015" = X2015,
+         "2016" = X2016,
+         "2017" = X2017,
+         "2018" = X2018,
+         "2019" = X2019)
 
 #Combine columns
 Depth <- Depth %>% gather(starts_with("2"), key = "year", value = "depth")
