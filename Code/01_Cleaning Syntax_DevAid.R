aa3c5fe670c2c00c92f9b9a9a3597fdabe0ff5dc
YOUR FULL NAME <milika.robbins@gmail.com>
2020-01-09 18:21:30 -0500

    Fixed Dev Aid, yay

--- Code/01_Cleaning Syntax_DevAid.R
+++ Code/01_Cleaning Syntax_DevAid.R
@@ -9,7 +9,7 @@ DevAid <- read.csv(here("data", "Cleaning_OECDFAO_DevAid_Data.csv"))
 DevAid <- select(DevAid, -Domain, -Element, -Element.Code, -Item, -Year.Code, -Unit, -Flag, -Flag.Description, -Note, -Domain.Code, -Item.Code, -Purpose)
 #Remember the flow is in 2016 USD
 
-#Combine the different purpose
+#Combine the different purposes
 DevAid <- DevAid %>% spread(Purpose.Code, Value)
 DevAid[is.na(DevAid)] <- 0
 DevAid <- rename(DevAid, prgm1 = "12240")
@@ -17,17 +17,24 @@ DevAid <- rename(DevAid, prgm2 = "52010")
 DevAid <- DevAid %>% mutate(donorflow = prgm1 + prgm2)
 DevAid <- select(DevAid, -prgm1, -prgm2)
 
-#Spread the donor types
-DevAid <- DevAid %>% spread(type, donorflow)
-
-#Rename Variables - incomplete
+#Rename Variables
 DevAid <- rename(DevAid, country = Recipient.Country)
 DevAid <- rename(DevAid, country_code = Recipient.Country.Code)
-DevAid <- rename(DevAid, type = Donor)
 DevAid <- rename(DevAid, donor_code = Donor.Code)
 DevAid <- rename(DevAid, year = Year)
+DevAid <- rename(DevAid, type = Donor)
 
+#Spread the donor types
+DevAid <- DevAid %>% spread(type, donorflow)
+DevAid <- DevAid %>% spread(type, donorflow)
+DevAid <- rename(DevAid, bilateral = "Bilateral Donors")
+DevAid <- rename(DevAid, multilateral = "Multilateral Donors")
+DevAid <- rename(DevAid, private = "Private Donors")
 
+coalesce_by_column <- function(df) {
+  return(coalesce(df[1], df[2]))
+}
+DevAid <- DevAid %>% group_by(country, year) %>% summarise_all(coalesce_by_column)
 
 #Initial Analysis
 diff(range(DevAid$flow, na.rm = FALSE))
