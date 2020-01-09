163114a2686b419d8354aa72a84ee746cc7de315
Bryce Leary <leary.bryce@gmail.com>
2020-01-09 13:36:32 -0500

    Updated merging syntax with new dataframes

--- Code/05_Merging syntax.R
+++ Code/05_Merging syntax.R
@@ -1,4 +1,13 @@
 #Merge code
 agriculture = merge(DevAid, FDIfinal, by = c("year", "country"))
+
 agriculture2 = merge(DevAid, FDIfinal, by = c("year", "country", "country_code"), suffixes = c("",""))
 #Warning message: In merge.data.frame(DevAid, FDIfinal, by = c("Year", "country"), :column names 'type' are duplicated in the result
+
+agriculture3 = merge(agriculture2, Nutrition, by = c("year", "country", "country_code"), suffixes = c("",""))
+#Warning message: In merge.data.frame(agriculture 2, Nutrition, by = c("year", "country", : column name 'type' is duplicated in the result
+
+agriculture4 = merge(agriculture3, Depth, by = c("year", "country"), suffixes = c("",""))
+#Warning message: In merge.data.frame(agriculture3, Depth, by = c("year", "country"), : column name 'type' is duplicated in the result
+
+agriculture5 = merge(agriculture4, Trade, by = )
