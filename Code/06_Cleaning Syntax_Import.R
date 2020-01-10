3832ecc68a758454bcd3ba414289068387078b83
Zeinabou Saidou Baraze <zeinab.baraze@gmail.com>
2020-01-09 20:39:54 -0500

    adding import data and syntax, and deleting combined data and syntax

---
+++ Code/06_Cleaning Syntax_Import.R
@@ -0,0 +1,29 @@
+#Set-up
+library(tidyverse)
+library(dplyr)
+library(here)
+library(magrittr)
+Import <- read.csv(here("data", "Cleaning_FAOSTAT_Import_Data.csv"))
+
+#Delete Unnecessary Variables
+Import <- select(Import, -Domain.Code, -Domain, -Element.Code, Item, -Item.Code, -Year.Code, -Unit, -Flag, -Flag.Description)
+Import <- select(Import, -Element, -Item)
+Import <- select(Import, -import_value)
+
+#Rename variables
+Import <- rename(Import, country_code = Area.Code)
+Import <- rename(Import, country = Area)
+Import <- rename(Import, import_value = Value)
+Import <- rename(Import, year = Year)
+
+#Multiply the value by 1000
+Import <- Import %>% mutate(imp_value =import_value * 1000)
+
+#Initial analysis by year
+year_agtrade_summ <- group_by(Trade, Year) %>% summarize("mean" = mean(agtrade, na.rm = TRUE), 
+                                                           "range" = diff(range(agtrade), na.rm = TRUE), 
+                                                           "median" = median(agtrade, na.rm = TRUE), 
+                                                           "sd" = sd(agtrade, na.rm = TRUE), 
+                                                           "25th" = quantile(agtrade, 0.25, na.rm = TRUE), 
+                                                           "75th" = quantile(agtrade, 0.75, na.rm = TRUE), 
+                                                           "IQR" = IQR(agtrade, na.rm = TRUE))
