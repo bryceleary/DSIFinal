2d32ea5cf034c2cdb2b201b788ded7353e0e71dc
YOUR FULL NAME <milika.robbins@gmail.com>
2020-01-09 19:06:52 -0500

    Updated FDI syntax to adjust 2010 USD to 2016 USD

--- Code/02_Cleaning Syntax_FDI.R
+++ Code/02_Cleaning Syntax_FDI.R
@@ -7,7 +7,6 @@ FDI <- read.csv(here("data", "Cleaning_FAOSTAT_FDI_Data.csv"))
 
 #Delete Variables
 FDI <- select(FDI, -Domain, -Element, -Element.Code, -Year.Code, -Unit, -Item.Code, -Flag, -Flag.Description, -Note)
-#Remember, the value is in 2010 USD
 
 #Rename Variables
 FDI <- rename(FDI, "flow" = Value)
@@ -17,8 +16,11 @@ FDI <- rename(FDI, "year" = Year)
 FDI <- rename(FDI, "type" = Domain.Code)
 FDI <- rename(FDI, "item" = Item)
 
+#Adjust USD from 2010 values to 2016 values
+FDI <- FDI %>% mutate(adj_flow = flow*(353.4/320.4))
+
 #Separate inflows and outflows
-FDIfinal <- FDI %>% spread(key = item, value = flow)
+FDIfinal <- FDI %>% spread(key = item, value = adj_flow)
 
 #Clean new variable structure and create netflow variable
 FDIfinal <- rename(FDIfinal, "t_fdi_in" = "Total FDI inflows")
