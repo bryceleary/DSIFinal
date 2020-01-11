e72d2fd58b1c25f71f924a167f2a1a1750e6d4bc
Bryce Leary <leary.bryce@gmail.com>
2020-01-10 22:49:15 -0500

    Updated clustering code and rmd file

--- Code/clustering.R
+++ Code/clustering.R
@@ -16,22 +16,22 @@ agcluster2015 <- agcluster %>% filter(year==2015)
 clust2000 <- dist(agcluster2000, method = "euclidian")
 den2000 <- hclust(clust2000, method = "complete")
 plot(den2000, labels = c("Burkina Faso", "Ghana", "Guinea", "Mali", "Mauritania", 
-                         "Niger",  "Senegal", "Sierra Leone"))
+                         "Niger",  "Senegal", "Sierra Leone"), main = "Clusters in 2000")
 
 clust2005 <- dist(agcluster2005, method = "euclidian")
 den2005 <- hclust(clust2005, method = "complete")
 plot(den2005, labels = c("Burkina Faso", "CÃ´te d'Ivoire", "Ghana", "Guinea-Bissau", 
                          "Guinea", "Liberia", "Mali", "Mauritania", 
-                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"))
+                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"), main = "Clusters in 2005")
 
 clust2010 <- dist(agcluster2010, method = "euclidian")
 den2010 <- hclust(clust2010, method = "complete")
 plot(den2010, labels = c("Benin", "Burkina Faso", "CÃ´te d'Ivoire", "Ghana", "Guinea-Bissau", 
                          "Guinea", "Liberia", "Mali", "Mauritania", 
-                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"))
+                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"), main = "Clusters in 2010")
 
 clust2015 <- dist(agcluster2015, method = "euclidian")
 den2015 <- hclust(clust2015, method = "complete")
 plot(den2015, labels = c("Benin", "Burkina Faso", "CÃ´te d'Ivoire", "Ghana", "Guinea-Bissau", 
                          "Guinea", "Liberia", "Mali", "Mauritania", 
-                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"))
+                         "Niger",  "Nigeria",  "Senegal", "Sierra Leone", "Togo"), "Clusters in 2015")
